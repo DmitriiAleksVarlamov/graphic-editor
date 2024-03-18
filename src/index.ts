0debfcd410cfdbdcc8cc0ce60d42010621dbb32c
Vasiay <VasilyRog1@yandex.ru>
2024-03-19 00:13:32 +0300

    feat: add multer

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import gm from 'gm';
 import path from 'node:path';
 import express from 'express';
 import { ROOT } from './constants';
+import multer from 'multer';
+
+const upload = multer({ dest: 'uploads/' });
 const PORT = 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-	console.log({url: path.resolve(ROOT, 'assets', 'sample_image.jpeg')});
+	console.log({ url: path.resolve(ROOT, 'assets', 'sample_image.jpeg') });
 
 	const readStream = fs.createReadStream(
 		path.resolve(ROOT, 'assets', 'sample_image.jpeg')
@@ -26,9 +29,18 @@ app.get('/', (req, res) => {
 		});
 });
 
-app.post('/image', (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
+
+	console.log({ image: req.file });
 	console.log('request');
 	res.send('End');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error, req, res, next) => {
+	console.log({ error });
+	res.status = 500;
+	res.send(`Server error was happend ${error.message}`);
 });
 
 app.listen(PORT, () => {
